66de58d2a89e6c6f4898b285dd052e7e804244b7
jibeeshms <jibeesh.ms@gmail.com>
2018-09-13 14:46:39 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>Test App</div>, document.getElementById('root'));
+registerServiceWorker();
